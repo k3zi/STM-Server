fd24e2af31df61a90781b7b0ca36a306cc57c595
Kesi Maduka <me@kez.io>
2016-02-21 01:00:26 -0600

    test db

--- public_html/stream_f7b44cfafd5c52223d5498196c8a2e7b.php
+++ public_html/stream_f7b44cfafd5c52223d5498196c8a2e7b.php
@@ -4,7 +4,9 @@ require_once('../includes/includes.php');
 $streamAlphaID = $_GET['streamID'];
 $streamID = $hashids->decode($streamAlphaID);
 $stream = streamInfoForID($streamID);
+print_r($streamID);
 print_r($stream);
+die();
 
 if((int)$stream['private'] == 1 && strlen($stream['passcode']) > 0){
 	if(isset($_POST['passcode'])){
