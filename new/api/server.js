5cf97972b72a6dfaed035cba1e7949de6ae2b23e
Kesi Maduka <me@kez.io>
2016-04-23 11:40:32 -0500

    remove db line

--- new/api/server.js
+++ new/api/server.js
@@ -486,12 +486,10 @@ app.get('/live/:hashed', function(req, res) {
         });
 
         xsocket.on('streamData', function(data) {
-            if (clientRelationship != false) {
-                if((Date.secNow() - data.time) < 1.0 || (Date.secNow() - lastSkip) < 15.0) {
-                    res.write(new Buffer(data.data, 'base64'));
-                } else {
-                    lastSkip = Date.secNow();
-                }
+            if ((Date.secNow() - data.time) < 1.0 || (Date.secNow() - lastSkip) < 15.0) {
+                res.write(new Buffer(data.data, 'base64'));
+            } else {
+                lastSkip = Date.secNow();
             }
         });
     }
