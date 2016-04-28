4ca06a42a8e62d81c5083f92dc20ef8032c06141
Kesi Maduka <me@kez.io>
2016-04-28 00:01:44 -0500

    log 500 error

--- new/api/server.js
+++ new/api/server.js
@@ -835,6 +835,7 @@ app.use(function(req, res) {
 
   // Handle 500
 app.use(function(error, req, res, next) {
+    console.log(error);
     res.status(500);
     return res.json(outputError('500: Internal Server Error'));
  });
