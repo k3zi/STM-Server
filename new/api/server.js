531a1d5defaff17f8b55386503ea059fd21dcb70
Kesi Maduka <me@kez.io>
2016-05-03 00:36:55 -0500

    attempt to order the lastMessage

--- new/api/server.js
+++ new/api/server.js
@@ -1013,6 +1013,7 @@ app.get('/v1/messages/list', jsonParser, urlEncodeHandler, sessionAuth, function
 
     var cypher = "MATCH (user: User)-[:joined]->(convo: Conversation)<-[:on]-(lastMessage: Message)"
     + " WHERE id(user) = {userID}"
+    + " ORDER BY lastMessage.date DESC";
     + " WITH DISTINCT convo, user, LAST(COLLECT(lastMessage)) AS lastMessage"
     + " OPTIONAL MATCH (users: User)-[:joined]->(convo)"
     + " RETURN convo, lastMessage, COLLECT(users) AS users"
