006bd7ece6c347508e9bfb2df18af0d901617f9a
Kesi Maduka <me@kez.io>
2016-05-08 17:13:43 -0500

    fix misspelled exports

--- api/helpers/index.js
+++ api/helpers/index.js
@@ -3,18 +3,18 @@ var Promise = require('promise');
 var config = require(process.argv[2] == 'dev' ? '../config/dev' : '../config/prod');
 var hasher = new Hashids(config.hash.salt, config.hash.minLength, config.hash.characters);
 
-export.encodeStr = function(str) {
+exports.encodeStr = function(str) {
     return hasher.encode(parseInt(str));
 }
 
-export.outputError = function(error) {
+exports.outputError = function(error) {
     return {
         'success': false,
         'error': error
     };
 }
 
-export.outputResult = function(result) {
+exports.outputResult = function(result) {
     return {
         'success': true,
         'result': result
