9283a8e66c7abeb6494f07e0024add10be7477d5
Kesi Maduka <me@kez.io>
2016-05-09 13:12:22 -0500

    test code

--- api/routes/v1/user.js
+++ api/routes/v1/user.js
@@ -6,21 +6,7 @@ var middlewares = require(config.directory.api + '/middlewares');
 var winston = require('winston');
 
 router.post('/authenticate', middlewares.auth, function(req, res) {
-    var data = req.body;
-    winston.debug('Login request received: ' + data.username);
 
-    userModel.find({username: data.username, password: data.password}).then(function(results) {
-        return new Promise(function (fulfill, reject) {
-        if (!results || results.length == 0) {
-            return reject('Invalid username/password');
-        }
-
-        var user = results[0];
-        req.session.user = user;
-        ensureExists(getUserDir(user.id), function(err) {
-            res.json(outputResult(user));
-        });
-    });
 });
 
-module.exports = router
+module.exports = router;
