470dc240eb9241942b6261231cc5ed714abe5426
Kesi Maduka <me@kez.io>
2016-05-08 22:00:18 -0500

    fix directory

--- api/test/routes.userSpec.js
+++ api/test/routes.userSpec.js
@@ -2,7 +2,7 @@ var should = require('should');
 var assert = require('assert');
 var request = require('supertest');
 var winston = require('winston');
-var config = require('./config/dev');
+var config = require('../config/dev');
 
 describe('/authenticate', function () {
     it('should return 200 OK');
