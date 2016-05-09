619f97cba6d503bb9cf8dd726ba1293b6b9808ff
Kesi Maduka <me@kez.io>
2016-05-08 21:55:41 -0500

    fix files

--- api/test/routes.js
+++ api/test/routes.js
@@ -2,7 +2,7 @@ var should = require('should');
 var assert = require('assert');
 var request = require('supertest');
 var winston = require('winston');
-var config = require('./config/dev');
+var config = require('../config/dev');
 
 var version = config.versions[config.versions.length - 1];
 var url = config.baseURL + version;
