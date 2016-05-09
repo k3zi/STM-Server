56a754d58b2c76a75a8e7857abaf8c0154dca19d
Kesi Maduka <me@kez.io>
2016-05-09 08:58:56 -0500

    fix logging

--- api/routes/v1/user.js
+++ api/routes/v1/user.js
@@ -3,13 +3,18 @@ var router = express.Router();
 var config = require('config');
 var userModel = require(config.directory.api + '/models/user');
 var middlewares = require(config.directory.api + '/middlewares');
+var winston = require('winston');
+
+winston.level = 'debug';
 
 router.post('/authenticate', middlewares.auth, function(req, res) {
     var data = req.body;
-    userModel.find({username: data.username, password: data.password}, 'User', function(err, results) {
+    winston.debug('Login request received: ' + data.username);
+
+    userModel.find({username: data.username, password: data.password}).then(function(results) {
+        return new Promise(function (fulfill, reject) {
         if (!results || results.length == 0) {
-            console.log(err);
-            return res.json(outputError('Invalid username/password'));
+            return reject('Invalid username/password');
         }
 
         var user = results[0];
