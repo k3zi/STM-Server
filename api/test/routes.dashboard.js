04f0d65a444201b735a6b5571200862e0b457f22
Kesi Maduka <me@kez.io>
2016-05-10 22:45:19 -0500

    use chai

--- api/test/routes.dashboard.js
+++ api/test/routes.dashboard.js
@@ -1,5 +1,5 @@
-var should = require('should');
-var assert = require('assert');
+var should = require('chai').should();
+var expect = require('chai').expect();
 var request = require('supertest');
 var winston = require('winston');
 var config = require('../config/dev');
@@ -10,10 +10,13 @@ request = request(url);
 
 describe('GET /', function () {
     it('should have the an arry of the dashboards contents', function(done) {
-        config.test.loginRequest(request.post('/')).end(function(err, res) {
-            assert.equal(res.body.success, 1);
-            assert(res.body.result.constructor === Array);
-            done();
+        config.test.loginRequest(request.get('/'))
+            .excpect(200)
+            .end(function(err, res) {
+                if(err) return done(err);
+                expect(res.body.success).to.equal(1);
+                expect(res.body.result.constructor).to.equal(Array);
+                done();
         });
     });
 });
--- api/test/routes.user.js
+++ api/test/routes.user.js
@@ -1,5 +1,5 @@
-var should = require('should');
-var assert = require('assert');
+var should = require('chai').should();
+var expect = require('chai').expect();
 var request = require('supertest');
 var winston = require('winston');
 var config = require('../config/dev');
@@ -10,9 +10,13 @@ request = request(url);
 
 describe('POST /authenticate', function () {
     it('should return error when provided nothing', function(done) {
-        config.test.authRequest(request.post('/authenticate')).end(function(err, res) {
-            assert.equal(res.body.success, 0);
-            done();
+        config.test.authRequest(request.post('/authenticate')).send({})
+            .excpect(200)
+            .end(function(err, res) {
+                if(err) return done(err);
+                expect(res.body.success).to.equal(1);
+                expect(res.body.result.username).to.equal(config.test.login.username);
+                done();
         });
     });
 });
