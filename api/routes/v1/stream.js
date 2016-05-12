095e4d11348e00d3e477a8e1e77362c69f30f405
Kesi Maduka <me@kez.io>
2016-05-12 00:53:57 -0500

    add debugging

--- api/routes/v1/stream.js
+++ api/routes/v1/stream.js
@@ -20,10 +20,13 @@ router.get('/:streamID/isOnline', middlewares.auth, function(req, res) {
     helpers.checkID(streamID).then(streamModel.streamLastOnline).then(function(lastOnline) {
         logger.debug('last online: ' + lastOnline);
         if (lastOnline < 10) {
+            logger.debug('online: 1');
             res.json(outputResult({'online': 1}));
         } else if (lastOnline < 45) {
+            logger.debug('online: 2');
             res.json(outputResult({'online': 2}));
         } else {
+            logger.debug('online: 0');
             res.json(outputResult({'online': 0}));
         }
     }).catch(function(err) {
