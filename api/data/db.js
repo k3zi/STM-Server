124e95ff79779b2096c0b7ae43dc0a922dd03c59
Kesi Maduka <me@kez.io>
2016-10-08 16:20:26 -0400

    fix db promise issue

--- api/data/db.js
+++ api/data/db.js
@@ -2,4 +2,15 @@ var seraph = require("seraph");
 var Promise = require('bluebird');
 var config = require('config');
 
-exports = Promise.promisifyAll(seraph(config.db));
+var db = seraph(config.db);
+
+var rel = {};
+rel.read = Promise.promisify(db.rel.read);
+rel.update = Promise.promisify(db.rel.update);
+
+exports.save = Promise.promisify(db.save);
+exports.find = Promise.promisify(db.find);
+exports.query = Promise.promisify(db.query);
+exports.read = Promise.promisify(db.read);
+exports.relate = Promise.promisify(db.relate);
+exports.rel = rel;
